ce02c8882cd10de225e78d0a8f1449b2fd2cb062
Mohammadreza Farhadi <farhadimrf@gmail.com>
2023-08-03 12:05:39 +0330

    improve player on library songs

--- components/Library.tsx
+++ components/Library.tsx
@@ -6,6 +6,7 @@ import { useUser } from "@/hooks/useUser";
 import useUploadModal from "@/hooks/useUploadModal";
 import { Song } from "@/types";
 import MediaItem from "./MediaItem";
+import useOnPlay from "@/hooks/useOnPlay";
 
 interface LibraryProps {
   songs: Song[];
@@ -15,6 +16,8 @@ const Library: React.FC<LibraryProps> = ({ songs }) => {
   const authModal = useAuthModal();
   const uploadModal = useUploadModal();
   const { user } = useUser();
+
+  const onPlay = useOnPlay(songs);
 
   const onClick = () => {
     if (!user) return authModal.onOpen();
@@ -38,7 +41,13 @@ const Library: React.FC<LibraryProps> = ({ songs }) => {
       </div>
       <div className="mt-4 flex flex-col gap-y-2 px-3">
         {songs.map((item) => (
-          <MediaItem key={item.id} data={item} onClick={() => {}} />
+          <MediaItem
+            key={item.id}
+            data={item}
+            onClick={(id: string) => {
+              onPlay(id);
+            }}
+          />
         ))}
       </div>
     </div>
