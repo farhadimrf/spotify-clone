21be0e1f8a99955bc89f42b6e3267dc1c7048120
Mohammadreza Farhadi <farhadimrf@gmail.com>
2023-08-09 15:37:51 +0330

    interacts with the Stripe API and the Supabase

--- libs/supabaseAdmin.ts
+++ libs/supabaseAdmin.ts
@@ -1,18 +1,20 @@
+// Import necessary dependencies and modules.
 import Stripe from "stripe";
 import { createClient } from "@supabase/supabase-js";
-
 import { Database } from "@/types_db";
 import { Price, Product } from "@/types";
-
 import { stripe } from "./stripe";
 import { toDateTime } from "./helpers";
 
+// Set up connection to the Supabase database.
 export const supabaseAdmin = createClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL || "",
   process.env.SUPABASE_SERVICE_ROLE_KEY || ""
 );
 
+// Function to upsert product data into Supabase "products" table.
 const upsertProductRecord = async (product: Stripe.Product) => {
+  // Prepare product data for insertion/update.
   const productData: Product = {
     id: product.id,
     active: product.active,
@@ -22,12 +24,15 @@ const upsertProductRecord = async (product: Stripe.Product) => {
     metadata: product.metadata,
   };
 
+  // Upsert product data into Supabase.
   const { error } = await supabaseAdmin.from("products").upsert([productData]);
   if (error) throw error;
   console.log(`Product inserted/updated: ${product.id}`);
 };
 
+// Function to upsert price data into Supabase "prices" table.
 const upsertPriceRecord = async (price: Stripe.Price) => {
+  // Prepare price data for insertion/update.
   const priceData: Price = {
     id: price.id,
     product_id: typeof price.product === "string" ? price.product : "",
@@ -42,11 +47,13 @@ const upsertPriceRecord = async (price: Stripe.Price) => {
     metadata: price.metadata,
   };
 
+  // Upsert price data into Supabase.
   const { error } = await supabaseAdmin.from("prices").upsert([priceData]);
   if (error) throw error;
   console.log(`Price inserted/updated: ${price.id}`);
 };
 
+// Function to create or retrieve a customer from Supabase and Stripe.
 const createOrRetrieveCustomer = async ({
   email,
   uuid,
@@ -54,12 +61,16 @@ const createOrRetrieveCustomer = async ({
   email: string;
   uuid: string;
 }) => {
+  // Check if customer with given UUID exists in Supabase.
   const { data, error } = await supabaseAdmin
     .from("customers")
     .select("stripe_customer_id")
     .eq("id", uuid)
     .single();
+
+  // If customer doesn't exist, create in Stripe and insert into Supabase.
   if (error || !data?.stripe_customer_id) {
+    // Prepare customer data for creation.
     const customerData: { metadata: { supabaseUUID: string }; email?: string } =
       {
         metadata: {
@@ -67,6 +78,8 @@ const createOrRetrieveCustomer = async ({
         },
       };
     if (email) customerData.email = email;
+
+    // Create customer in Stripe and insert into Supabase.
     const customer = await stripe.customers.create(customerData);
     const { error: supabaseError } = await supabaseAdmin
       .from("customers")
@@ -78,16 +91,22 @@ const createOrRetrieveCustomer = async ({
   return data.stripe_customer_id;
 };
 
+// Function to copy billing details to customer in Stripe and Supabase.
 const copyBillingDetailsToCustomer = async (
   uuid: string,
   payment_method: Stripe.PaymentMethod
 ) => {
-  //Todo: check this assertion
+  // Extract customer ID and billing details.
   const customer = payment_method.customer as string;
   const { name, phone, address } = payment_method.billing_details;
+
+  // If billing details are complete, update customer in Stripe and Supabase.
   if (!name || !phone || !address) return;
+
   //@ts-ignore
   await stripe.customers.update(customer, { name, phone, address });
+
+  // Update user's billing details in Supabase.
   const { error } = await supabaseAdmin
     .from("users")
     .update({
@@ -98,12 +117,13 @@ const copyBillingDetailsToCustomer = async (
   if (error) throw error;
 };
 
+// Function to manage subscription status changes.
 const manageSubscriptionStatusChange = async (
   subscriptionId: string,
   customerId: string,
   createAction = false
 ) => {
-  // Get customer's UUID from mapping table.
+  // Retrieve customer's UUID from Supabase mapping table.
   const { data: customerData, error: noCustomerError } = await supabaseAdmin
     .from("customers")
     .select("id")
@@ -113,10 +133,12 @@ const manageSubscriptionStatusChange = async (
 
   const { id: uuid } = customerData!;
 
+  // Retrieve subscription details from Stripe.
   const subscription = await stripe.subscriptions.retrieve(subscriptionId, {
     expand: ["default_payment_method"],
   });
-  // Upsert the latest status of the subscription object.
+
+  // Prepare subscription data for insertion/update in Supabase.
   const subscriptionData: Database["public"]["Tables"]["subscriptions"]["Insert"] =
     {
       id: subscription.id,
@@ -125,7 +147,7 @@ const manageSubscriptionStatusChange = async (
       // @ts-ignore
       status: subscription.status,
       price_id: subscription.items.data[0].price.id,
-      //TODO check quantity on subscription
+      // TODO check quantity on subscription
       // @ts-ignore
       quantity: subscription.quantity,
       cancel_at_period_end: subscription.cancel_at_period_end,
@@ -153,6 +175,7 @@ const manageSubscriptionStatusChange = async (
         : null,
     };
 
+  // Upsert subscription data into Supabase.
   const { error } = await supabaseAdmin
     .from("subscriptions")
     .upsert([subscriptionData]);
@@ -161,8 +184,8 @@ const manageSubscriptionStatusChange = async (
     `Inserted/updated subscription [${subscription.id}] for user [${uuid}]`
   );
 
-  // For a new subscription copy the billing details to the customer object.
-  // NOTE: This is a costly operation and should happen at the very end.
+  // If creating a new subscription, copy billing details to customer.
+  // NOTE: This operation is costly and should be done at the end.
   if (createAction && subscription.default_payment_method && uuid)
     //@ts-ignore
     await copyBillingDetailsToCustomer(
@@ -171,6 +194,7 @@ const manageSubscriptionStatusChange = async (
     );
 };
 
+// Export the functions for use in other parts of the application.
 export {
   upsertProductRecord,
   upsertPriceRecord,
